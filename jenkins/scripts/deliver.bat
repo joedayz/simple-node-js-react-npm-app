a3eed823f91f98e17c1d562af719309924452045
José Díaz <jose.diaz@joedayz.pe>
2025-03-19 00:07:38 -0500

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,12 +1,16 @@
 @echo off
 echo The following "npm" command builds your Node.js/React application for production...
-call npm run build
+npm run build
 
 echo The following "npm" command runs your Node.js/React application in development mode...
-start /B npm start
-timeout /T 1 /NOBREAK
-for /f "tokens=2 delims==" %%I in ('wmic process where "name='node.exe'" get ProcessId /value') do set PID=%%I
-echo %PID% > .pidfile
+:: Ejecutar en una nueva ventana
+start "React App" cmd /c npm start
+
+:: Esperar 1 segundo antes de continuar
+timeout /t 1 >nul
+
+:: Guardar el PID del proceso en .pidfile (opcional, ya que Windows maneja procesos de forma diferente)
+echo %ERRORLEVEL% > .pidfile
 
 echo Now...
 echo Visit http://localhost:3000 to see your Node.js/React application in action.
